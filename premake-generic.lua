2874f2ea79dd247a8001af89f9fe3c54c115f237
Eric Parayre <eric.parayre@gmail.com>
2016-02-07 16:59:58 -0500

    creating a json file representing the workspace

--- premake-generic.lua
+++ premake-generic.lua
@@ -1,16 +1,31 @@
 -- generic
 
-premake.modules.generic {}
+premake.modules.generic = {}
 
 local m = premake.modules.generic
+local p = premake
 
 newaction {
     trigger = "generic",
     description = "Generate a Generic project.",
 
     onStart = function()
-        print("testing Generic action!")
+        p.indent('  ')
+    end,
+
+    onWorkspace = function(wks)
+        p.generate(wks, ".json", m.GenerateWorkspace)
     end
 }
+
+function m.GenerateWorkspace(wks)
+    p.w('{')
+    p.push()
+
+    p.x('"workspace": "%s"', wks.name)
+
+    p.pop()
+    p.w('}')
+end
 
 return m
